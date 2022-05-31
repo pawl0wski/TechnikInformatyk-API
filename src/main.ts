136e5591e6d62cc65d1e7cc51c08ef912e28d508
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-05-31 17:24:58 +0200

    Run express on app start

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import * as dotenv from "dotenv";
+import express from "express";
 import DatabaseService from "./database/databaseService";
 dotenv.config();
 
 let databaseService = new DatabaseService();
+const PORT = process.env.SERVER_PORT || 3000;
+
+const app = express();
+
+async function init() {
+    await databaseService.sync();
+    app.listen(PORT, () => {
+        console.log(`App started http://localhost:${PORT}`);
+    });
+}
+
+init();
