4ca382c8383d2b025f727721193a6ec64ad9e3a5
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-06-01 17:47:18 +0200

    Change webpack config to production

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
     entry: "./src/main.ts",
-    mode: "development",
+    mode: "production",
     target: "node",
     devtool: false,
     externals: ["pg-hstore"],
