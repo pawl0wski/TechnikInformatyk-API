5bc3d99b6f7d6acfc80969487a5e3d72dc91f08b
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-06-02 00:24:40 +0200

    Fix tests

--- tests/cdn/cdn.test.ts
+++ tests/cdn/cdn.test.ts
@@ -2,29 +2,25 @@ import CDN from "../../src/cdn/cdn";
 
 describe("CDN", () => {
     describe("isCDNEnabled", () => {
-        let cdn: CDN;
-        beforeAll(() => {
-            cdn = new CDN({});
-        });
         test('Should return true if env is "true"', () => {
             process.env.ENABLE_CDN = "true";
-            expect(cdn.isCDNEnabled).toBe(true);
+            expect(CDN.isCDNEnabled).toBe(true);
         });
         test('Should return true if env is "1"', () => {
             process.env.ENABLE_CDN = "1";
-            expect(cdn.isCDNEnabled).toBe(true);
+            expect(CDN.isCDNEnabled).toBe(true);
         });
         test('Should return false if env is "false"', () => {
             process.env.ENABLE_CDN = "false";
-            expect(cdn.isCDNEnabled).toBe(false);
+            expect(CDN.isCDNEnabled).toBe(false);
         });
         test('Should return false if env is "0"', () => {
             process.env.ENABLE_CDN = "0";
-            expect(cdn.isCDNEnabled).toBe(false);
+            expect(CDN.isCDNEnabled).toBe(false);
         });
         test("Should return false if env is undefined", () => {
             delete process.env.ENABLE_CDN;
-            expect(cdn.isCDNEnabled).toBe(false);
+            expect(CDN.isCDNEnabled).toBe(false);
         });
     });
     describe("getCDNPath", () => {
