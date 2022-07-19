b0dfa28e44beb0ba82807932cbf0781f52c3bc15
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-19 23:00:57 +0200

    Remove restore and delete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import * as dotenv from "dotenv";
 import { Command } from "commander";
-import restoreCommand from "./commands/restore";
-import backupCommand from "./commands/backup";
 import rebuildCDN from "./commands/rebuildCDN";
 import serverCommand from "./commands/server";
 import generateKeyCommand from "./commands/generateKey";
@@ -10,18 +8,6 @@ dotenv.config();
 const program = new Command();
 
 program.name("Technik Informatyk API");
-
-program
-    .command("restore")
-    .description("Restore all data by json files.")
-    .argument("<string>", "directory with all json files")
-    .action(restoreCommand);
-
-program
-    .command("backup")
-    .description("Backup all data to json files.")
-    .argument("<string>", "output directory")
-    .action(backupCommand);
 
 program
     .command("rebuildCDN")
