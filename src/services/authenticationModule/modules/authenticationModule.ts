27dc627dd6ec0e3fc8bf408d434bf77b47acbedd
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-22 17:02:11 +0200

    Add authenticationModule

---
+++ src/services/authenticationModule/modules/authenticationModule.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import AuthenticationError from "../../../errors/authenticationError";
+import AuthenticationService from "../authenticationService";
+
+export async function authenticationModule(
+    request: express.Request,
+    securityName: string,
+    scopes?: string[]
+): Promise<any> {
+    if (securityName === "api_key") {
+        const service = new AuthenticationService();
+        const key = request.headers["authorization"];
+        if (key && key.length == 64) {
+            for (const permission of scopes ?? []) {
+                if (
+                    !(await service.checkIfKeyHavePermission(key, permission))
+                ) {
+                    throw new AuthenticationError(
+                        "The key has no permissions to access this content."
+                    );
+                }
+            }
+            return;
+        }
+        throw new AuthenticationError(
+            "The key, or the absence of a key, prevents you from accessing the content."
+        );
+    }
+}
