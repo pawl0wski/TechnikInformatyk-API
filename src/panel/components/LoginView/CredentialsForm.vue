7abc20a4f5144777fa9486d02fd5595bcc6e5d5c
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-28 23:20:35 +0200

    Add CredentialsForm

---
+++ src/panel/components/LoginView/CredentialsForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <form>
+        <div class="mb-3">
+            <label class="form-label">Authorization key</label>
+            <input
+                id="authKey"
+                v-model="authKey"
+                type="password"
+                class="form-control"
+            />
+        </div>
+        <div class="d-flex align-items-end">
+            <button type="submit" class="btn btn-primary ms-auto">Login</button>
+        </div>
+    </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    data(): { authKey: string } {
+        return {
+            authKey: "",
+        };
+    },
+});
+</script>
