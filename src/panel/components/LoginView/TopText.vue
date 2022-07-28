bea91efa43a24b538af2bcf2141034d4248eccc9
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-28 23:00:13 +0200

    Strech LoginView to full width when user use small screen

---
+++ src/panel/components/LoginView/TopText.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Login</h1>
+  <p class="text-muted">
+    Please enter the authorization code to go to the admin panel
+  </p>
+</template>
--- src/panel/views/LoginView.vue
+++ src/panel/views/LoginView.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-6 shadow-sm rounded-3 ">
+      <div class="col-12 col-md-6 shadow-sm rounded-3 ">
         <div class="login-dialog p-3">
-          <h1>Login</h1>
-          <p class="text-muted">
-            Please enter the authorization code to go to the admin panel
-          </p>
+          <TopText />
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+import TopText from "../components/LoginView/TopText.vue";
+export default {
+  components: { TopText }
+}
+</script>
