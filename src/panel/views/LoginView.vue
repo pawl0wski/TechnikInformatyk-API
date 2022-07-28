8e40daf4326c19c34b2509868b227145d651c358
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-28 23:19:12 +0200

    Reformat vue files

--- src/panel/views/LoginView.vue
+++ src/panel/views/LoginView.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-12 col-md-6 shadow-sm rounded-3 ">
-        <div class="login-dialog p-3">
-          <TopText />
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 shadow-sm rounded-3">
+                <div class="login-dialog p-3">
+                    <TopText />
+                    <CredentialsForm />
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
-<script>
+<script lang="ts">
 import TopText from "../components/LoginView/TopText.vue";
+import CredentialsForm from "../components/LoginView/CredentialsForm.vue";
+
 export default {
-  components: { TopText }
-}
+    components: { CredentialsForm, TopText },
+};
 </script>
