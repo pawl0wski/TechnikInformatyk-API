8eec1f215eae101977c26a77c78ff87cfa72dc2c
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-29 00:13:58 +0200

    Create go to login button for debugging

--- src/panel/router/router.ts
+++ src/panel/router/router.ts
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "../views/LoginView.vue";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {
+            path: "/panel/",
+            name: "Home",
+            component: HomeView,
+        },
         {
             path: "/panel/login",
             name: "Login",
---
+++ src/panel/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="btn btn-primary" @click="goToLoginView">Login</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import router from "../router/router";
+
+export default defineComponent({
+    methods: {
+        goToLoginView() {
+            router.push("/panel/login");
+        },
+    },
+});
+</script>
