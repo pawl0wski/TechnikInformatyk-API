2a7e6c03467cafb97cb941370523368947585c03
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-28 23:55:51 +0200

    Add actions and state to AuthStore

--- src/panel/stores/authStore.ts
+++ src/panel/stores/authStore.ts
@@ -1,5 +1,38 @@
 import { defineStore } from "pinia";
+import axios from "axios";
+import ApiKeyResponseI from "../../interfaces/apiKeyResponse";
+
+interface AuthState {
+    apiKey: string;
+    permission: string;
+    correct: boolean | null;
+}
 
 export const useAuthStore = defineStore({
     id: "auth",
+    state: (): AuthState => ({
+        apiKey: "",
+        permission: "",
+        correct: null,
+    }),
+    actions: {
+        setApiKey(apiKey: string) {
+            this.apiKey = apiKey;
+        },
+        async checkIfApiKeyIsCorrect() {
+            const apiResponse = await axios.get(`/key/${this.apiKey}`);
+
+            if (apiResponse.status === 404) this.correct = false;
+            this.correct = true;
+        },
+        async getApiKeyPermission() {
+            const apiResponse = await axios.get(`/key/${this.apiKey}`);
+
+            if (apiResponse.status === 404) {
+                this.permission = (
+                    apiResponse.data as ApiKeyResponseI
+                ).permission;
+            }
+        },
+    },
 });
