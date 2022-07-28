362c4db551708b9051ce1f4c5d00a009f8fa3636
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-28 22:58:55 +0200

    Add TopText to LoginView

--- src/panel/views/LoginView.vue
+++ src/panel/views/LoginView.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Login Page</h1>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-6 shadow-sm rounded-3 ">
+        <div class="login-dialog p-3">
+          <h1>Login</h1>
+          <p class="text-muted">
+            Please enter the authorization code to go to the admin panel
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
