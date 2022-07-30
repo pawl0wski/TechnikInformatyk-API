8e8938e906a3082e4df8cf042be3b8e261801632
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-30 23:04:31 +0200

    Change redirectIfNotValid link in AuthorizationValidator

--- src/panel/router/guard/validator/authorizationValidator.ts
+++ src/panel/router/guard/validator/authorizationValidator.ts
@@ -2,7 +2,7 @@ import GuardValidator from "../guardValidator";
 import { useAuthStore } from "../../../stores/authStore";
 
 export default class AuthorizationValidator extends GuardValidator {
-    redirectIfNotValid = "/panel/login";
+    redirectIfNotValid = "/login";
     validate(): boolean {
         const authStore = useAuthStore();
         return authStore.isKeyCorrect ?? false;
