20d37e933c87a1e469ef77aa3736dcbcc8bf9209
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-03 22:40:39 +0200

    Print the error to the console if it is handled by the errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -13,17 +13,21 @@ export default function errorHandler(
     next: NextFunction
 ) {
     if (err instanceof NotFoundError) {
+        console.error(err);
         res.status(404).json({
             message: err.message,
         } as ErrorResultI);
     } else if (err instanceof AuthenticationError) {
+        console.error(err);
         res.status(401).json({
             message: err.message,
         } as ErrorResultI);
     } else {
+        console.error(err);
         res.status(500).json({
             message: "Internal Server Error",
         } as ErrorResultI);
     }
+
     next();
 }
