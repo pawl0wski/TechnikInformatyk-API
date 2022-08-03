8147a0e3578890ea4d5c68870a943b0ff62f7569
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-08-03 23:02:56 +0200

    Add ValidateError to errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import NotFoundError from "../errors/notFoundError";
 import AuthenticationError from "../errors/authenticationError";
+import { ValidationError } from "sequelize";
+import { ValidateError } from "@tsoa/runtime";
 
 export interface ErrorResultI {
     message: string;
@@ -24,6 +26,12 @@ export default function errorHandler(
             error: err,
             message: err.message,
         });
+    } else if (err instanceof ValidateError) {
+        sendStatus(res, {
+            statusCode: err.status,
+            message: err.fields,
+            error: err,
+        });
     } else if (err instanceof Error) {
         sendStatus(res, {
             statusCode: 500,
@@ -37,7 +45,11 @@ export default function errorHandler(
 
 function sendStatus(
     res: Response,
-    statusOptions: { statusCode: number; error: Error; message: string }
+    statusOptions: {
+        statusCode: number;
+        error: Error;
+        message: string | object;
+    }
 ) {
     const { statusCode, error, message } = statusOptions;
     console.error(error);
