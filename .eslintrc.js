a51388fe87b6202e513fbeac39b65d65bb88ffa2
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-09-19 13:58:56 +0200

    Remove Vue depedecies and configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,23 +5,15 @@ module.exports = {
     },
     extends: [
         "eslint:recommended",
-        "plugin:vue/vue3-recommended",
         "plugin:@typescript-eslint/recommended",
     ],
-    parser: "vue-eslint-parser",
     parserOptions: {
         parser: "@typescript-eslint/parser",
         sourceType: "module",
     },
-    plugins: ["@typescript-eslint", "eslint-plugin-vue"],
+    plugins: ["@typescript-eslint"],
     rules: {
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-empty-function": "off",
-        "vue/html-indent": "off",
-        "vue/html-self-closing": "off",
-        "vue/max-attributes-per-line": "off",
-        "vue/singleline-html-element-content-newline": "off",
-        "vue/comment-directive": "off",
-        "vue/html-closing-bracket-newline": "off",
     },
 };
